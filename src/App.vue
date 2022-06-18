ad47c71e536e94612fa582bbb4371d65b8d92c19
KaliemSB <mycael590@gmail.com>
2022-06-18 11:46:55 -0300

    chore: container, header and card components

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <Header></Header>
+  <Container style="margin-top: 1rem;">
+    <Card/>
+  </Container>
+</template>
+
+<script lang="ts" setup>
+import Header from "./components/header.vue";
+import Card from "./components/card.vue";
+import Container from "./components/container.vue";
+</script>
+
+<style lang="scss">
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+  font-family: 'Nunito Sans', sans-serif;
+}
+
+body {
+  background: var(--background);
+  height: 100vh;
+}
+
+:root {
+  --background: hsl(0, 0%, 98%);
+  --input: hsl(0, 0%, 52%);
+  --element: hsl(0, 0%, 100%);
+  --text: hsl(200, 15%, 8%);
+}
+
+:root.dark-theme {
+  --background: hsl(207, 26%, 17%);
+  --input: hsl(209, 23%, 22%);
+  --element: hsl(209, 23%, 22%);
+  --text: hsl(0, 0%, 100%);
+}
+</style>
---
+++ src/components/card.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <img src="https://flagcdn.com/de.svg" alt="DE">
+    <div class="wrapper">
+      <h3>Germany</h3>
+      <div class="details">
+        <p><strong>Population:</strong> 81,770,900</p>
+        <p><strong>Region:</strong> Europe</p>
+        <p><strong>Capital:</strong> Berlin</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  div {
+    display: inline-flex;
+    flex-direction: column;
+    border-radius: 4px;
+    background: white;
+
+    img {
+      width: 16rem;
+      height: auto;
+      border-radius: 4px 4px 0 0;
+    }
+
+    .wrapper {
+      padding: 1rem;
+
+      h3 {
+        color: var(--text);
+        font-weight: 800;
+        font-size: 1.2rem;
+      }
+
+      .details {
+        font-size: .8rem;
+      }
+    }
+  }
+</style>
